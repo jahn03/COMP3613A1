b35f592074a75d9e752598ccc97183a36d50d3e8
jahn03 <jahnvisooknanan@gmail.com>
2024-09-29 23:15:38 +0000

    user.py in controller and wsgi.py updated

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,6 +1,7 @@
-from App.models import User
+from App.models import User, Competition, Result
 from App.database import db
 
+# user functions
 def create_user(username, password):
     newuser = User(username=username, password=password)
     db.session.add(newuser)
@@ -31,3 +32,79 @@ def update_user(id, username):
         return db.session.commit()
     return None
     
+#-------------------------------------------
+
+# competition functoins
+
+def create_competition(name, date, description):
+    new_competition = Competition(name=name, date=date, description=description)
+    db.session.add(new_competition)
+    db.session.commit()
+    return new_competition
+
+def ger_all_competitions():
+    return Competition.query.all()
+
+def get_all_competitions_json():
+    competitions = Competition.query.all()
+    if not competitions:
+        return []
+    competitions = [competition.get_json() for competition in competitions]
+    return competitions
+
+def get_competition_by_id(competition_id):
+    return Competition.query.get(competition_id)
+
+
+# results functions
+
+def create_result(competition_id, student_name, score):
+    new_result = Result(competition_id=competition_id, student_name=student_name, score=score)
+    db.session.add(new_result)
+    db.session.commit()
+    return new_result
+
+def get_all_results():
+    return Result.query.all()
+
+def get_all_results_json():
+    results = Result.query.all()
+    if not results:
+        return []
+    results = [result.get_json() for result in results]
+    return results
+
+def get_results_by_competition(competition_id):
+    return Result.query.filter_by(competition_id=competition_id).all()
+
+def import_competitions_result_from_file(file_path):
+    file_abs_path = os.path.join('data', file_path)
+
+    try:
+        with open(file_abs_path, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+
+            for row in reader:
+                competition_id = int(row['competition_id'])
+                student_name = row['student_name']
+                score = int(row['score'])
+
+                new_result = Result(
+                    competition_id=competition_id,
+                    student_name=student_name,
+                    score=score
+                )
+
+                db.session.add(new_result)
+
+            db.session.commit()
+            print(f"Results imported from {file_path}")
+    
+    except FileNotFoundError:
+        print(f"File not found")
+    
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+#-------------------------------------------
--- wsgi.py
+++ wsgi.py
@@ -2,10 +2,10 @@ import click, pytest, sys
 from flask import Flask
 from flask.cli import with_appcontext, AppGroup
 
-from App.database import db, get_migrate
+from App.database import db, get_migrate, get_all_competitions
 from App.models import User
 from App.main import create_app
-from App.controllers import ( create_user, get_all_users_json, get_all_users, initialize )
+from App.controllers import ( create_user, get_all_users_json, get_all_users, create_competition, get_all_competitions, get_all_competitions_json, create_result, get_all_results, get_all_results_json, import_competition_results_from_file, initialize )
 
 
 # This commands file allow you to create convenient CLI commands for testing controllers
@@ -25,7 +25,7 @@ User Commands
 
 # Commands can be organized using groups
 
-# create a group, it would be the first argument of the comand
+# create a group, it would be the first argument of the command
 # eg : flask user <command>
 user_cli = AppGroup('user', help='User object commands') 
 
@@ -49,6 +49,70 @@ def list_user_command(format):
 
 app.cli.add_command(user_cli) # add the group to the cli
 
+#-----------------------------------------------------------------------------
+# COMPETITION COMMANDS
+
+competition_cli = AppGroup('competition', help='Competition object commans')
+
+@competition_cli.command("create", help="Creates competition")
+@click.argument("name")
+@click.argument("date")
+@click.argument("description")
+
+# 1: create competition
+def create_competition_command(name, date, description):
+    create_competition(name, date, description)
+    print(f'Competiition {name} created!')
+
+# 3: view competitions list
+@competition_cli.command("list", help="View competitions list")
+@click.argument("format", default="string")
+
+def list_competition_command(format):
+    if format == 'string':
+        print(get_all_competitions())
+    else:
+        print(get_all_competitions_json())
+
+app.cli.add_command(competition_cli)
+
+
+
+
+# RESULT COMMANDS
+
+result_cli = AppGroup('result', help='Result object commans')
+
+@result_cli.command("create", help="Creates a result")
+@click.argument("competition_id")
+@click.argument("student_name")
+@click.argument("score")
+
+# create results
+def create_result_command(competition_id, student_name, score):
+    create_result(competition_id, student_name, score)
+    print(f'Result for {student_name} created')
+
+# 4: View competition results
+@result_cli.command("list", help="Lists competition results")
+@click.argument("format", default="string")
+
+def list_result_command(format):
+    if format == 'string':
+        print(get_all_results())
+    else:
+        print(get_all_results_json())
+
+# 2: Import competition results from file
+@result_cli.command("import", help="Import competition results from file")
+@click.argument("file_path")
+
+def import_result_command(file_path):
+    import_competition_results_from_file(file_path)
+    print(f'Competition results imported from {file_path}')
+
+app.cli.add_command(result_cli)
+#-----------------------------------------------------------------------------
 '''
 Test Commands
 '''
