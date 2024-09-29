ba9e3a3a75ec3fa65bbd20000669c49545944aed
jahn03 <jahnvisooknanan@gmail.com>
2024-09-29 23:13:53 +0000

    user.py in models updated

--- App/models/user.py
+++ App/models/user.py
@@ -24,3 +24,47 @@ class User(db.Model):
         """Check hashed password."""
         return check_password_hash(self.password, password)
 
+#--------------------------------------------------------------------------
+
+class Competition(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(225), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
+    def __init__(self, name, date, description):
+        self.name = name
+        self.date = date
+        self.description = description
+
+    def get_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'date': self.date.isoformat(),
+            'description': self.description
+        }
+
+class Result(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    competition_id = db.Column(db.Integer, db.ForeignKey('competition_id'), nullable=False)
+    student_name = db.Column(db.String(255), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+
+    competition = db.relationship('Competition', backref=db.backref('results', lazy=True))
+
+def __init__(self, competition_id, student_name, score):
+    self.competition_id = competition_id
+    self.student_name = student_name
+    self.score = score
+
+def get_json(self):
+    return {
+        'id': self.id,
+        'competition_id': self.competition_id,
+        'student_name': self.student_name,
+        'score': self.score
+    }
+
+
+#--------------------------------------------------------------------------
